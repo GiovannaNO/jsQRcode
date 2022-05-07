3de7490cf879ee9970e625e08cc72b5778431ecf
GiovannaNO <giovanna10nunes@gmail.com>
2022-05-07 10:16:02 +0900

    Código 3

--- www/components/comandos.js
+++ www/components/comandos.js
@@ -9,9 +9,11 @@ window.onload = function(){
                 }else if(result.text == '989895555'){
                     document.querySelector("#dados1").innerHTML = "<hr><h2>Informações do produto escaneado:</h2><p><strong>Nome do produto:</strong> Samsung Galaxy J8 Dual Chip 4g 64gb Tela 6</p>";
                     document.getElementById("img").src = 'img/Imagem2.jpg';
-                    document.querySelector("#dados2").innerHTML = "<p><strong>Valor:</strong> <em>R$ 1.888,00</em></p><p><strong>Velocidade do Processador:</strong> 1.8GHz</p><p><strong>Tipo de Processador:</strong> OctaCore</p><p><strong>Tamanho (Tela Principal):</strong> 153.6mm (6.0'')</p><p><strong>Resolução da câmera:</strong><br>-Câmera Traseira: 16.0 MP + 5.0 MP</p><table><tr><td>Memória RAM:</td><td>4GB</td></tr><tr><td>Memória Total Interna</td><td>64GB</td></tr><tr><td>Memória Disponível</td><td>50.6GB</td></tr></table>";
+                    document.querySelector("#dados2").innerHTML = "<p><strong>Valor:</strong> <em>R$ 1.888,00</em></p><p><strong>Velocidade do Processador:</strong> 1.8GHz</p><p><strong>Tipo de Processador:</strong> OctaCore</p><p><strong>Tamanho (Tela Principal):</strong> 153.6mm (6.0'')</p><p><strong>Resolução da câmera:</strong><br>-Câmera Traseira: 16.0 MP + 5.0 MP</p><table><tr><td>Memória RAM</td><td>4GB</td></tr><tr><td>Memória Total Interna</td><td>64GB</td></tr><tr><td>Memória Disponível</td><td>50.6GB</td></tr></table>";
                 }else if(result.text == '85236987'){
-                    
+                    document.querySelector("#dados1").innerHTML = "<hr><h2>Informações do produto escaneado:</h2><p><strong>Nome do produto:</strong> Motorola One XT1941 Branco 64GB</p>";
+                    document.getElementById("img").src = 'img/Imagem3.jpg';
+                    document.querySelector("#dados2").innerHTML = "<p><strong>Valor:</strong> <em>⨂</em></p><p><strong>Sistema operacional:</strong> Android 8.1 Oreo</p><p><strong>Processador:</strong> Qualcomm Snapdragon 625 (MSM8953) Octa-Core de 2.0 GHz</p><p><strong>Memória interna:</strong> 64 GB Total (parte usada para o sistema operacional e aplicativos pré-instalados)</p><h5>Características Gerais:</h5><ul><li>- Tela Max Vision e design ao máximo</li><li>- Câmera dupla e armazenamento ilimitado</li><li>- Android One: primeiro do Brasil, atualizado e seguro</li><li>- Carregamento ultrarrápido TurboPower</li><li>- Últimas inovações do Google incorporadas</li><li>- Leitor Biométrico (Impressão Digital)</li></ul><table><tr><td>Memória RAM</td><td>4GB</td></tr><tr><td>Tecnologia GSM</td><td>3G e 4G</td></tr><tr><td>Cor</td><td>Branco</td></tr></table><br>";
                 }
             },
 
